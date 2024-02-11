1651c8774f8da04e88f40ead346be1a8c0c9ecc5
aman <53443872+aman-raza@users.noreply.github.com>
2024-02-11 21:53:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
+from ui import QuizInterface
 
 question_bank = []
 
@@ -12,8 +13,5 @@ for data in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-while quiz.still_has_questions():
-    quiz.next_question()
+quiz_ui = QuizInterface(quiz)
 
-print("You have completed the quiz.")
-print(f"Your final score is: {quiz.score}/{quiz.question_number}")
