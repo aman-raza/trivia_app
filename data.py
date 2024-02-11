d1f4c27a23803b31c667966eba247544f657cae4
aman <53443872+aman-raza@users.noreply.github.com>
2024-02-11 17:08:48 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,10 @@
 import requests
 
-response = requests.get("https://opentdb.com/api.php?amount=10&type=boolean").json()
+BASE_URL = "https://opentdb.com/api.php"
+parameters = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+response = requests.get(url=BASE_URL, params=parameters).json()
 question_data = response["results"]
