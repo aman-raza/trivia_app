2b8f0f6ebc22d112540025b698022397fd740b6d
aman <53443872+aman-raza@users.noreply.github.com>
2024-02-11 21:52:54 +0530

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -6,25 +6,23 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
         self.score = 0
+        self.current_question = None
 
     def next_question(self):
-        current_question = self.question_list[self.question_number]
+        self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        q_text = html.unescape(current_question.text)
-        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer)
+        q_text = html.unescape(self.current_question.text)
+        return f"Q.{self.question_number}: {q_text}"
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
-    def check_answer(self, user_answer, correct_answer):
+    def check_answer(self, user_answer):
+        correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"The correct answer was: {correct_answer}")
-        print(f"Your current score is: {self.score}/{self.question_number}")
+            return False
 
 
