313e27fea814d532f31bf70158b8a0d3856ad194
aman <53443872+aman-raza@users.noreply.github.com>
2024-02-11 17:18:02 +0530

    Update quiz_brain.py

--- quiz_brain.py
+++ quiz_brain.py
@@ -1,3 +1,6 @@
+import html
+
+
 class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
@@ -7,7 +10,8 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        q_text = html.unescape(current_question.text)
+        user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
         self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
